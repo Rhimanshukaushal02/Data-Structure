6c1f1cb7aaa06decc4f7fbc65a6d2a820165b9bc
Rhimanshukaushal02 <63235919+Rhimanshukaushal02@users.noreply.github.com>
2020-04-06 19:56:49 +0530

    Update inorder-postorer-preorder.c

--- inorder-postorer-preorder.c
+++ inorder-postorer-preorder.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+void create(struct node*)
 void InOrder(struct node *);
 void PostOrder(struct node*);
 void PreOrder(struct node*);
@@ -10,6 +11,38 @@ struct node
     struct node *right;
 };
 struct node *root=NULL;
+int main()
+{
+    root=create();
+    printf("Preorder is:\n");
+    PreOrder(root);
+    printf("Inorder is:\n");
+    InOrder(root);
+    printf("Postorder is:\n");
+
+}
+void(struct node *create)
+{
+    struct node *newnode;
+    int x;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("enter the data(-1 for no data)");
+    scanf("%d",&x);
+    if(x==-1)
+    {
+
+        return 0;
+    }
+    else
+    {
+        newnode->data=x;
+        printf("enter the left node data of %d",x);
+        newnode->left=create();
+        printf("enter the right node data of %d",x);
+        newnode->right=create();
+        return newnode;
+    }
+}
 void InOrder(struct node *root)
 {
     if(root==NULL)
